9706a89887292ae5f9e9c47780c411329bca0e17
seah78 <webmaster@seah.dev>
2023-11-06 15:14:53 +0100

    save

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(2)
 paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
@@ -25,7 +25,7 @@ paddle_a.goto(-350, 0)
 
 # Paddle B
 paddle_b = turtle.Turtle()
-paddle_b.speed(0)
+paddle_b.speed(2)
 paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
@@ -34,7 +34,7 @@ paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0)
+ball.speed(1000)
 ball.shape("square")
 ball.color("white")
 ball.penup()
@@ -75,8 +75,8 @@ def paddle_b_down():
 
 # Keyboard bindings
 wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_a_up, "a")
+wn.onkeypress(paddle_a_down, "q")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
